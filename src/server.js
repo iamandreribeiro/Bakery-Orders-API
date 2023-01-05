9317c0f194fbea0d87e57e49852e918dea20caa2
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-01-05 17:29:02 -0300

    import routes in server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import clientsRoute from "./routes/clientsRoute.js";
+import cakesRoute from "./routes/cakesRoute.js";
+import ordersRoute from "./routes/ordersRoute.js";
+
 
 const app = express();
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+
+app.use(clientsRoute);
+app.use(cakesRoute);
+app.use(ordersRoute);
 
 const port = process.env.PORT;
 
