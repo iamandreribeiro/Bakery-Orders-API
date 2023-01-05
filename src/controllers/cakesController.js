1af42c4ab0725def382caecfc89d8f48005640df
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-01-05 17:54:05 -0300

    creating new cake

--- src/controllers/cakesController.js
+++ src/controllers/cakesController.js
@@ -1,3 +1,31 @@
-export async function postCake(req, res) {
+import { connectionDB } from "../database/db.js";
 
+export async function postCake(req, res) {
+    const name = res.locals.data.name;
+    const price = res.locals.data.price;
+    const image = res.locals.data.image;
+    const description = res.locals.data.description;
+
+    try {
+        const queryCake = await connectionDB.query(
+            `SELECT * FROM cakes
+            WHERE name=$1`,
+            [name]
+        );
+
+        if(queryCake.rows.length > 0) {
+            return res.sendStatus(409);
+        } else {
+            await connectionDB.query(
+                `INSERT INTO cakes
+                (name, price, image, description)
+                VALUES ($1, $2, $3, $4)`,
+                [name, price, image, description]
+            );
+
+            return res.sendStatus(201);
+        }
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 };
